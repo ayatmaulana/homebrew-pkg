e1438bf70372296f917e4bf36a2df399f0f3333b
Ayat Maulana <ayatmaulana.04@gmail.com>
2020-09-01 15:42:53 +0700

    update to v0.0.4

--- tada-runner-generator.rb
+++ tada-runner-generator.rb
@@ -3,8 +3,8 @@
 class TadaRunnerGenerator < Formula
   desc "Generator for TADA Runner, simplify you life and avoiding copas from existing runner."
   homepage ""
-  url "https://github.com/ayatmaulana/tada-runner-generator/releases/download/v0.0.3/tada-runner-generator-darwin-amd64.tar.gz"
-  sha256 "50f6e1256f2719358f27a1d059d2d2c6cb74235a018f7f5ac86ee1fe293d79d4"
+  url "https://github.com/ayatmaulana/tada-runner-generator/releases/download/v0.0.4/tada-runner-generator-darwin-amd64.tar.gz"
+  sha256 "02a0046879029a147085996ec1ccb2b23b5e263879212fad0e47e56ae4fe48a9"
   license ""
 
   def install
